05d411e0e2e6f650f473bef898c6505402e2e819
vladimir <vladimir.borovic@gmail.com>
2015-04-23 14:54:31 +0300

    refactoring

--- dbmanage/admin.py
+++ dbmanage/admin.py
@@ -6,10 +6,16 @@ Created on Dec 23, 2013
 from django.contrib import admin
 from dbmanage.models import *
 
+class RegisteredInline(admin.StackedInline):
+    model = Registration
+    extra = 0
+    readonly_fields = ['date']
 
 class DatabaseAdmin(admin.ModelAdmin):
     list_filter = ('status', 'is_perpetual', 'max_registrations', )
     search_fields = ['^database_id']
+    inlines = [RegisteredInline]
+
 
 admin.site.register(Database, DatabaseAdmin)
 admin.site.register(Updating)
